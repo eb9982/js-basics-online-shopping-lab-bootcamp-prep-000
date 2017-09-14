c8e0b0a367538a3dd2c445f4863c04aa3546ab0c
Luke Borsare <lborsare1@gmail.com>
2017-09-14 23:35:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,43 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  function getrandnum() {
+     return Math.random() * 100
+  }
+  var itemName = item
+  var itemPrice = parseInt(getrandnum())
+  var newitemforcart = { [itemName]:itemPrice }
+  cart.push(newitemforcart)
+  console.log(`${itemName} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  itemarr = []
+  pricearr = []
+  for (var i = 0; i < cart.length; i++) {
+    pricearr.push(Object.values(cart[i]))
+    itemarr.push(Object.keys(cart[i]))
+  }
+  return itemarr
+  if(cart.length === 0){
+      console.log('Your shopping cart is empty.')
+    }
+  else {
+    firstcart = []
+    lastcart = []
+    for (var i = 0; i < cart.length; i++) {
+      if (i < cart.length - 1) {
+        for (var itemName in cart[i]){
+          firstcart.push(itemName + 'at' + cart[i][itemName])
+      }
+
+      else {
+        lastcart.push(itemName + 'at' + cart[i][itemName])
+      }
+    }
+    return firstcart
+  }
 }
 
 function total() {
